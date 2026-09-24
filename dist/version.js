ba840da9304d404e81c77c5119787f5711638707
MostafaAdly <MostafaAdly.official@gmail.com>
2026-09-24 17:02:46 +0300

    2.6.1

--- dist/version.js
+++ dist/version.js
@@ -4,5 +4,5 @@
  * Written by scripts/sync-version.mjs from package.json, which runs before
  * build and bundle. Change the version with `npm version <level>`.
  */
-export const VERSION = '2.6.0';
+export const VERSION = '2.6.1';
 //# sourceMappingURL=version.js.map
--- src/version.ts
+++ src/version.ts
@@ -5,4 +5,4 @@
  * build and bundle. Change the version with `npm version <level>`.
  */
 
-export const VERSION = '2.6.0';
+export const VERSION = '2.6.1';
